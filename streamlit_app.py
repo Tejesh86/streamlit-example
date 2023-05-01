89d76df1ef8f9fa2de4794eef65cf149dc7f59af
Tejesh86 <121223428+Tejesh86@users.noreply.github.com>
2023-05-01 20:12:17 +0530

    Update streamlit_app.py
    Display multi select

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 # streamlit.dataframe(my_fruit_list);
 # Let's put a list here so they can pick the fruit they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index));
+# display the table on the page
+streamlit.dataframe(my_fruit_list);
