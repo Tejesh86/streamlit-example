aa61b7a7907c8ae6ad521d4acb4ff7f12eff3c69
Tejesh86 <121223428+Tejesh86@users.noreply.github.com>
2023-05-01 15:03:22 +0530

    Update streamlit_app.py
    added icon

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My snowflake bedge 2 practice');
-streamlit.header('Snowflake');
-streamlit.text('streamlit text');
-streamlit.text('trying to solve error');
+streamlit.title('🥣 My snowflake bedge 2 practice');
+streamlit.header('🥗 Snowflake');
+streamlit.text('🐔 streamlit text');
+streamlit.text('🥑 trying to solve error');
