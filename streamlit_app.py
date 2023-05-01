f5b99b33bb844b3999708a2f761c896d04cee38d
Tejesh86 <121223428+Tejesh86@users.noreply.github.com>
2023-05-01 20:28:43 +0530

    Update streamlit_app.py
    show selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,13 @@ import streamlit
 import pandas
 streamlit.title('🥣 My snowflake bedge 2 practice');
 streamlit.header('🥗 Snowflake');
-streamlit.text('🐔 streamlit text');
+# streamlit.text('🐔 streamlit text');
 streamlit.text('🥑 trying to solve error');
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
-my_fruit_list = my_fruit_list.set_index('Fruit');
+# my_fruit_list = my_fruit_list.set_index('Fruit');
 # streamlit.dataframe(my_fruit_list);
 # Let's put a list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Lemon','Lime']);
 # display the table on the page
 fruits_to_show = my_fruit_list.loc[fruits_selected];
-streamlit.dataframe(my_fruit_list);
+streamlit.dataframe(fruits_to_show);
