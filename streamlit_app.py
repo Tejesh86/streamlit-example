999dbdfea3237089cf1bd63b58b3589a5046995b
Tejesh86 <121223428+Tejesh86@users.noreply.github.com>
2023-05-01 15:15:32 +0530

    Update streamlit_app.py
    imported pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
-
+import pandas
 streamlit.title('🥣 My snowflake bedge 2 practice');
 streamlit.header('🥗 Snowflake');
 streamlit.text('🐔 streamlit text');
 streamlit.text('🥑 trying to solve error');
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+streamlit.dataframe(my_fruit_list);
