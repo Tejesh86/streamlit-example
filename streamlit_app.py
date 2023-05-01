38633f0070c3abea8de073b9783f20592b3654f0
Tejesh86 <121223428+Tejesh86@users.noreply.github.com>
2023-05-01 15:01:08 +0530

    Update streamlit_app.py
    typo solved error

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('My snowflake bedge 2 practice');
 streamlit.header('Snowflake');
 streamlit.text('streamlit text');
-streamlt.text('trying to solve error');
+streamlit.text('trying to solve error');
