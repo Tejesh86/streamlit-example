1485d094c47db9bd42524ab1f522755ff8f05dc7
Tejesh86 <121223428+Tejesh86@users.noreply.github.com>
2023-05-13 14:35:55 +0530

    Update streamlit_app.py
    Changed 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 streamlit.title('🥣 My snowflake bedge 2 practice');
 streamlit.header('🥗 Snowflake');
 # streamlit.text('🐔 streamlit text');
-streamlit.text('🥑 trying to solve error');
+streamlit.text('🥑 trying to resolve error');
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
 my_fruit_list = my_fruit_list.set_index('Fruit');
 # streamlit.dataframe(my_fruit_list);
